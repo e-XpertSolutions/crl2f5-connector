a4614a9618340bafa5691714ffbfd8664a94ca29
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2017-09-04 11:20:52 +0200

    main: minor refactoring +doc

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
+// crl2f5-connector is a service that continuously fetches a CRL at a given
+// interval, uploads it on a F5 BigIP and updates the related client SSL
+// profile.
+//
+// For usage information, please see:
+//    crl2f5-connector -h
+//    crl2f5-connector -help
+//
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -17,22 +26,18 @@ const (
 	bugfix = "0"
 )
 
+// Print usage and exit with status 1.
 func usage() {
 	fmt.Fprintf(os.Stderr, "usage: %s\n", filepath.Base(os.Args[0]))
 	flag.PrintDefaults()
 	os.Exit(1)
 }
 
+// Print version number and exit with status 0.
 func version() {
 	fmt.Printf("%s - %s.%s.%s\n", filepath.Base(os.Args[0]), major, minor, bugfix)
 	os.Exit(0)
 }
-
-var (
-	configPath   = flag.String("config", "config.toml", "path to configuration file")
-	verboseMode  = flag.Bool("verbose", false, "enable verbose mode")
-	printVersion = flag.Bool("version", false, "print current version and exit")
-)
 
 // Hide stderr and stout behind an io.Writer in order to ease testing.
 var (
@@ -45,20 +50,64 @@ var exit = func(status int) {
 	os.Exit(status)
 }
 
+// fatal prints to standard error output (stderr) and then exit the program with
+// a status 1. The prefix "fatal:" is prepended to the message. Arguments are
+// handled in the manner of fmt.Print.
 func fatal(v ...interface{}) {
 	fmt.Fprintln(stderr, "fatal:", fmt.Sprint(v...))
 	exit(1)
 }
 
+// verbose prints to standard output (stdout) only when the verboseMode is
+// enabled. Otherwise it does nothing. The prefix "verbose:" is prepended to the
+// message. Arguments are handled in the manner of fmt.Print.
 func verbose(v ...interface{}) {
 	if *verboseMode {
 		fmt.Fprintln(stdout, "verbose:", fmt.Sprint(v...))
 	}
 }
 
+// info prints to standard output (stdout). The prefix "info:" is prepended to
+// the message. Arguments are handled in the manner of fmt.Print.
 func info(v ...interface{}) {
 	fmt.Fprintln(stdout, "info:", fmt.Sprint(v...))
 }
+
+// initF5Client initializes a new f5.Client with the provided configuration.
+func initF5Client(cfg f5Config) (*f5.Client, error) {
+	var (
+		f5Client *f5.Client
+		err      error
+	)
+	switch authMethod := cfg.AuthMethod; authMethod {
+	case "basic":
+		f5Client, err = f5.NewBasicClient(cfg.URL, cfg.User, cfg.Password)
+	case "token":
+		f5Client, err = f5.NewTokenClient(
+			cfg.URL,
+			cfg.User,
+			cfg.Password,
+			cfg.LoginProviderName,
+			cfg.SSLCheck,
+		)
+	default:
+		err = errors.New("unsupported auth method \"" + authMethod + "\"")
+	}
+	if err != nil {
+		return nil, err
+	}
+	if !cfg.SSLCheck {
+		f5Client.DisableCertCheck()
+	}
+	return f5Client, nil
+}
+
+// Command line flags.
+var (
+	configPath   = flag.String("config", "config.toml", "path to configuration file")
+	verboseMode  = flag.Bool("verbose", false, "enable verbose mode")
+	printVersion = flag.Bool("version", false, "print current version and exit")
+)
 
 func main() {
 	flag.Usage = usage
@@ -72,31 +121,14 @@ func main() {
 	if err != nil {
 		fatal(err)
 	}
-
-	if cfg.CRL == nil || len(cfg.CRL) == 0 {
+	if !cfg.hasCRLDistributionPoint() {
 		fatal("no crl distribution point provided in the configuration file")
 	}
 
-	var f5Client *f5.Client
-	switch authMethod := cfg.F5.AuthMethod; authMethod {
-	case "basic":
-		f5Client, err = f5.NewBasicClient(cfg.F5.URL, cfg.F5.User, cfg.F5.Password)
-	case "token":
-		f5Client, err = f5.NewTokenClient(
-			cfg.F5.URL,
-			cfg.F5.User,
-			cfg.F5.Password,
-			cfg.F5.LoginProviderName,
-			cfg.F5.SSLCheck,
-		)
-	default:
-		fatal("unsupported auth method \"", authMethod)
-	}
+	f5Client, err := initF5Client(cfg.F5)
 	if err != nil {
+		fatal(err)
 		fatal("cannot initialize f5 client: ", err)
-	}
-	if !cfg.F5.SSLCheck {
-		f5Client.DisableCertCheck()
 	}
 
 	p := new(pool)
@@ -107,7 +139,7 @@ func main() {
 		fatal("cannot start workers: ", err)
 	}
 
-	// Catch OS signal.
+	// Catch OS signal to gracefully shutdown the program.
 	sigChan := make(chan os.Signal, 2)
 	signal.Notify(sigChan, os.Interrupt, os.Kill)
 
--- main_test.go
+++ main_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"os/exec"
 	"testing"
@@ -59,6 +62,13 @@ const wantUsage = `usage: crl2f5-connector.test
     	enable verbose mode
   -version
     	print current version and exit
+`
+
+var f5TokenResp = `{
+	"token": {
+		"token": "5C0F982A0BF37CBE5DE2CB8313102A494A4759E5704371B77D7E35ADBE4AAC33184EB3C5117D94FAFA054B7DB7F02539F6550F8D4FA25C4BFF1145287E93F70D"
+	}
+}
 `
 
 func TestUsage(t *testing.T) {
@@ -172,4 +182,102 @@ func TestInfo(t *testing.T) {
 	if got := stdoutBuf.String(); got != want {
 		t.Errorf("info(%q): got %q; want %q", "test", got, want)
 	}
+}
+
+func TestInitF5Client(t *testing.T) {
+	t.Run("Happy Path With Basic Auth", testInitF5ClientHappyPathWithBasicAuth)
+	t.Run("Happy Path With Token Auth", testInitF5ClientHappyPathWithTokenAuth)
+	t.Run("Fail Token Auth", testInitF5ClientFailTokenAuth)
+	t.Run("Fail Unsupported Auth Method", testInitF5ClientUnsupportedAuthMethod)
+}
+
+func testInitF5ClientHappyPathWithBasicAuth(t *testing.T) {
+	cfg := f5Config{
+		AuthMethod: "basic",
+		URL:        "http://localhost/bigip",
+		User:       "admin",
+		Password:   "admin",
+	}
+	f5Client, err := initF5Client(cfg)
+	if err != nil {
+		t.Fatalf("initF5Client(): unexpected error %q", err.Error())
+	}
+	req, err := f5Client.MakeRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatalf("initF5Client().MakeRequest(): unexpected error %q", err.Error())
+	}
+	if got := req.URL.String(); got != cfg.URL+"/test" {
+		t.Errorf("initF5Client().MakeRequest(): got url %q; want %q", got, cfg.URL+"/test")
+	}
+	wantAuthorization := "Basic YWRtaW46YWRtaW4="
+	if got := req.Header.Get("Authorization"); got != wantAuthorization {
+		t.Errorf("initF5Client().MakeRequest(): got authorization header %q; want %q", got, wantAuthorization)
+	}
+}
+
+func testInitF5ClientHappyPathWithTokenAuth(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(f5TokenResp))
+	}))
+	defer ts.Close()
+
+	cfg := f5Config{
+		AuthMethod: "token",
+		URL:        ts.URL,
+		User:       "admin",
+		Password:   "admin",
+	}
+	f5Client, err := initF5Client(cfg)
+	if err != nil {
+		t.Fatalf("initF5Client().MakeRequest(): unexpected error %q", err.Error())
+	}
+	req, err := f5Client.MakeRequest("GET", "/test", nil)
+	if err != nil {
+		t.Fatalf("initF5Client().MakeRequest(): unexpected error %q", err.Error())
+	}
+	if got := req.URL.String(); got != cfg.URL+"/test" {
+		t.Errorf("initF5Client().MakeRequest(): got url %q; want %q", got, cfg.URL+"/test")
+	}
+	wantToken := "5C0F982A0BF37CBE5DE2CB8313102A494A4759E5704371B77D7E35ADBE4AAC33184EB3C5117D94FAFA054B7DB7F02539F6550F8D4FA25C4BFF1145287E93F70D"
+	if got := req.Header.Get("X-F5-Auth-Token"); got != wantToken {
+		t.Fatalf("initF5Client().MakeRequest(): got X-F5-Auth-Token header %q; want %q", got, wantToken)
+	}
+}
+
+func testInitF5ClientFailTokenAuth(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "some error", http.StatusBadRequest)
+	}))
+	defer ts.Close()
+
+	cfg := f5Config{
+		AuthMethod: "token",
+		URL:        ts.URL,
+		User:       "admin",
+		Password:   "admin",
+	}
+	_, err := initF5Client(cfg)
+	if err == nil {
+		t.Fatal("initF5Client(): expected error, got nil")
+	}
+	wantErr := "failed to create token client (token negociation failed): http status 400 Bad Request"
+	if got := err.Error(); got != wantErr {
+		t.Errorf("initF5Client(): got error %q; want %q", got, wantErr)
+	}
+}
+
+func testInitF5ClientUnsupportedAuthMethod(t *testing.T) {
+	authMethod := "something else that does not exist"
+	cfg := f5Config{
+		AuthMethod: authMethod,
+	}
+	_, err := initF5Client(cfg)
+	if err == nil {
+		t.Fatal("initF5Client(): expected error, got nil")
+	}
+	wantErr := fmt.Sprintf("unsupported auth method \"%s\"", authMethod)
+	if got := err.Error(); got != wantErr {
+		t.Errorf("initF5Client(): got error %q; want %q", got, wantErr)
+	}
+
 }
